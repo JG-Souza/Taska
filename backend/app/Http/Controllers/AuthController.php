160e64ac8288b32445fc15dd54d7d7b176268e7d
JG-Souza <joaogsouzafma@gmail.com>
2025-06-29 16:55:50 -0300

    initial commit

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->validated();
+
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => new UserResource($user),
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->only('email', 'password');
+
+        if (!Auth::attempt($data)) {
+            return response()->json([
+                'message' => 'Credenciais inválidas'
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth-token')->plainToText;
+
+        return response()->json([
+            'token' => $token,
+            'user' => new UserResource($user),
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'logout realizado com sucesso.'
+        ]);
+    }
+}
